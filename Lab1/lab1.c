4ddcd7b73204e5eb638fb8213ed58b09efb26033
VitalTerrorX <vitalterrorx@gmail.com>
2021-02-08 23:44:22 +0700

    lab1

--- Lab1/lab1.c
+++ Lab1/lab1.c
@@ -29,21 +29,38 @@ int main(int argc, char **argv) {
 				(void) setpgid(0, 0);
 				break;
 			case 'p':
+				printf("pid = %ld\n", getpid());
+				printf("parent pid = %ld\n", getppid());
+				printf("group pid = %ld\n", getpgid(0));
 				break;
 			case 'U':
+				if (ulimit(UL_SETFSIZE, atol(optarg)) == -1) {
+					perror("Must be super-user to increase ulimit\n");
+				}
 				break;
 			case 'u':
+				printf("ulimit = %ld\n", ulimit(UL_GETFSIZE));
 				break;
 			case 'c':
+				getrlimit(RLIMIT_CORE, &rlp);
+				printf("core size = %ld\n", rlp.rlim_cur);
 				break;
 			case 'C':
+				getrlimit(RLIMIT_CORE, &rlp);
+				rlp.rlim_cur = atol(optarg);
+				if (setrlimit(RLIMIT_CORE, &rlp) == -1)
+					perror("Must be super-user to increase core\n");
 				break;
 			case 'd':
 				printf("current working directiry is: %s\n", getcwd(NULL, 100));
 				break;
 			case 'v':
+				printf("environment variables are:\n");
+				for (currentEnvVar = environ; *currentEnvVar; currentEnvVar++)
+					printf("%s\n", *currentEnvVar);
 				break;
 			case 'V':
+				putenv(optarg);
 				break;
 		}
 	}
