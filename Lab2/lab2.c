9b170827183ebf839f9404424c099ed688375ded
VitalTerrorX <vitalterrorx@gmail.com>
2021-02-15 19:03:03 +0700

    lab2

--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-//extern char *tzname[];
+
+extern char *tzname[];
 
 int main() {
     time_t now;
-    struct tm *sp;
+    struct tm *tm;
+
     putenv("TZ=PST");
     (void) time(&now);
-    printf("%s", ctime(&now));
-    sp = localtime(&now);
-    printf("%d/%d/%02d %d:%02d %s\n",
-           sp->tm_mon + 1, sp->tm_mday,
-           sp->tm_year, sp->tm_hour,
-           sp->tm_min, tzname[sp->tm_isdst]);
+    tm = localtime(&now);
+    printf("%s - %s\n", tzname[tm->tm_isdst], asctime(tm));
+
     exit(0);
 }
