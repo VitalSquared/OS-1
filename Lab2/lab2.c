ef954def70462e430b813c1c023a6cc2b1d64798
Vitaly Spirin <40485016+VitalTerrorX@users.noreply.github.com>
2021-02-16 09:27:27 +0700

    Update lab2.c

--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -12,15 +12,17 @@ int main() {
     if (setenv("TZ","PST8PDT",1) == -1) {
 	perror("Unable to set TZ");
     }
-    printf("%s", ctime(&now));     
+	
     (void) time(&now);
+    printf("%s", ctime(&now));     
+
     tm = localtime(&now);
     printf("%d/%d/%02d %d:%02d %s\n", 
 		    tm->tm_mon + 1, 
 		    tm->tm_mday, 
 		    tm->tm_year, 
-		    tm->tm_hour, tm
-		    ->tm_min, 
+		    tm->tm_hour, 
+	   	    tm->tm_min, 
 		    tzname[tm->tm_isdst]);
     exit(0);
 }
