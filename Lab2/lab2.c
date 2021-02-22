c9f02ca174923d4ad65498694e3b119dbff0d3e7
VitalTerrorX <vitalterrorx@gmail.com>
2021-02-23 00:38:39 +0700

    lab2

--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -5,12 +5,15 @@
 
 extern char *tzname[];
 
+#define ERR_SET_ENV_VAR -1
+
 int main() {
     time_t now;
     struct tm *tm;
 
-    if (setenv("TZ","PST8PDT",1) == -1) {
-	perror("Unable to set TZ");
+    int setenv_res = setenv("TZ", "PST8PDT", 1);
+    if (setenv_res == ERR_SET_ENV_VAR) {
+	perror("Unable to set TZ environment varialbe: ");
     }
 	
     (void) time(&now);
@@ -20,9 +23,9 @@ int main() {
     printf("%d/%d/%02d %d:%02d %s\n", 
 		    tm->tm_mon + 1, 
 		    tm->tm_mday, 
-		    tm->tm_year + 1900, 
+		    tm->tm_year + 1900,
 		    tm->tm_hour, 
-	   	    tm->tm_min, 
+		    tm->tm_min, 
 		    tzname[tm->tm_isdst]);
     exit(0);
 }
