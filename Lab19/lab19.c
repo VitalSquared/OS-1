7f6b160d598d9951b715aa0e245f841147ee24dc
VitalTerrorX <vitalterrorx@gmail.com>
2021-03-08 20:39:38 +0700

    lab19

--- Lab19/lab19.c
+++ Lab19/lab19.c
@@ -2,40 +2,58 @@
 
 #include <stdio.h>
 #include <sys/types.h>
+#include <limits.h>
 #include <dirent.h>
 #include <string.h>
 #include <errno.h>
+#include <stdlib.h>
 
-#define SIZE 256
+#define SIZE (NAME_MAX + 1)
+#define NO_ERROR 0
+
+extern int errno;
 
 int main(int argc, char **argv) {
     char pattern[SIZE];
-    struct dirent *p;
-    DIR *dir;
+    struct dirent *dp;
+    DIR *dirp;
     int i = 0, length, count = 0, j, patternLength;
     int flag = 0;
-    dir = opendir(".");
-    if (!dir) {
-         perror("opendir");
-         exit(2);
+
+    dirp = opendir(".");
+    if (dirp == NULL) {
+         perror("Can't open directory");
+         exit(0);
     }
+
     printf("Enter pattern: ");
     fgets(pattern, SIZE, stdin);
     patternLength = strlen(pattern) - 1;
     pattern[patternLength] = 0;
-    for (i = 0; patternLength > i; i++) {
+
+    for (i = 0; i < patternLength; i++) {
         if (pattern[i] == '/') {
-            printf("Symbol / is not supported pattern\n");
-            exit(1);
+            printf("Symbol '/' is not supported pattern\n");
+            exit(0);
         }
     }
-    while (0 != (p = readdir(dir))) {
-        length = strlen(p->d_name);
+
+    do {
+	dp = readdir(dirp);
+	if (dp == NULL) {
+		if (errno != NO_ERROR) {
+			perror("Can't read directory");
+		}
+		break;
+	}
+
+        length = strlen(dp->d_name);
         j = 0;
         flag = 0;
+
         for (i = 0; (length > i) && (patternLength > j); i++) {
             if (('?' != pattern[j]) && ('*' != pattern[j])) {
-                if (pattern[j] != p->d_name[i]) break;
+                if (pattern[j] != dp->d_name[i]) break;
                 j++;
             } else if ('?' == pattern[j]) {
                 j++;
@@ -53,7 +71,7 @@ int main(int argc, char **argv) {
                     continue;
                 }
                 while (length > i) {
-                    if (pattern[j] == p->d_name[i]) {
+                    if (pattern[j] == dp->d_name[i]) {
                         break;
                     }
                     i++;
@@ -61,6 +79,7 @@ int main(int argc, char **argv) {
                 j++;
             }
         }
+
         if (length == i) {
             while (patternLength > j) {
                 if ('*' != pattern[j])
@@ -69,15 +88,18 @@ int main(int argc, char **argv) {
             }
             if (patternLength == j) flag = 1;
         }
+
         if ((1 == flag) || (i == length && j == patternLength)) {
-            printf("%s\n", p->d_name);
+            printf("%s\n", dp->d_name);
             count++;
         }
-    }
-    if (!count) {
+
+    } while (1);
+
+    if (count == 0) {
         printf("Pattern was = %s\n", pattern);
     }
-    closedir(dir);
+    closedir(dirp);
     return 0;
 }
 
