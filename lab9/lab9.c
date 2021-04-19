4f9b9e90c5d314678e5faa7916cbcca599ef760c
VitalTerrorX <vitalterrorx@gmail.com>
2021-04-19 12:33:55 +0700

    lab9

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -5,47 +5,46 @@
 #include <unistd.h>
 
 #define ERROR_FORK -1
-#define ERROR_EXEC -1
+#define ERROR_EXECL -1
 #define ERROR_WAIT -1
 #define CHILD_PROCESS 0
 #define PARENT_PROCESS 1
 #define END_OF_ARGS NULL
 
 int main(int argc, char* argv[]) {
-    	if (argc < 2) {
-        	printf("Usage: %s file\n", argv[0]);
-        	return 0;
-    	}
+    if (argc < 2) {
+        printf("Usage: %s file\n", argv[0]);
+        return 0;
+    }
 
-    	pid_t fork_check = fork();
-    	if (fork_check == ERROR_FORK) {
-        	perror("Error while creating new process");
-        	return 0;
-    	}
+    pid_t fork_check = fork();
+    if (fork_check == ERROR_FORK) {
+        perror("Error while creating new process");
+        return 0;
+    }
 
-    	if (fork_check == CHILD_PROCESS) {
-        	printf("\nI am a new process\nMy PID: %d\nMy PPID:%d\n",getpid(),getppid());
-        	int execl_check = execl("/bin/cat", "cat", argv[1], END_OF_ARGS);
-        
-        	if(execl_check == ERROR_EXECL)
-        	{
-            		perror("Error while opening cat");
-            		return 0;
-        	}
-    	}
+    if (fork_check == CHILD_PROCESS) {
+        printf("\nI am a new process\nMy PID: %d\nMy PPID:%d\n", getpid(), getppid());
+        int execl_check = execl("/bin/cat", "cat", argv[1], END_OF_ARGS);
 
-    	if (fork_check >= PARENT_PROCESS) {
-        	printf("I am parent\nMy child's PID: %d\nMy PID: %d\n", fork_check, getpid());
-    	}
+        if (execl_check == ERROR_EXECL) {
+            perror("Error while opening cat");
+            return 0;
+        }
+    }
 
-	int status = 0;
-    	pid_t wait_check = wait(&status);
-    	if (wait_check == ERROR_WAIT) {
-        	perror("wait error");
-        	return 0;
-    	}
+    if (fork_check >= PARENT_PROCESS) {
+        printf("I am parent\nMy child's PID: %d\nMy PID: %d\n", fork_check, getpid());
+    }
 
-    	printf("Wait ended.\n");
-    	return 0;
+    int status = 0;
+    pid_t wait_check = wait(&status);
+    if (wait_check == ERROR_WAIT) {
+        perror("wait error");
+        return 0;
+    }
+
+    printf("Wait ended.\n");
+    return 0;
 }
 
