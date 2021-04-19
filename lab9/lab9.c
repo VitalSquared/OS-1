d9ad50ead2d9217914c187e51b99a29d4b2f8ba1
VitalTerrorX <vitalterrorx@gmail.com>
2021-04-19 22:40:58 +0700

    lab9

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -7,34 +7,34 @@
 #define ERROR_FORK -1
 #define ERROR_WAIT -1
 #define CHILD_PROCESS 0
+#define NUM_OF_ARGS_REQUIRED 2
 #define END_OF_ARGS NULL
 #define STATUS_IGNORE NULL
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
+    if (argc != NUM_OF_ARGS_REQUIRED) {
         printf("Usage: %s file\n", argv[0]);
-        return 0;
+        return EXIT_SUCCESS;
     }
 
     pid_t fork_check = fork();
     if (fork_check == ERROR_FORK) {
         perror("Error while creating new process");
-        return 0;
+        return EXIT_FAILURE;
     }
 
     if (fork_check == CHILD_PROCESS) {
         execl("/bin/cat", "cat", argv[1], END_OF_ARGS);
         perror("execl error");
-        return 0;
+        return EXIT_FAILURE;
     }
 
     pid_t wait_check = wait(STATUS_IGNORE);
     if (wait_check == ERROR_WAIT) {
         perror("Error while waiting");
-        return 0;
+        return EXIT_FAILURE;
     }
 
     printf("Random text that is written by parent\n");
-    return 0;
+    return EXIT_SUCCESS;
 }
-
