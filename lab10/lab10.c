c072e9e15df6aa264c3c8c3f1e88dc968874dd8d
VitalTerrorX <vitalterrorx@gmail.com>
2021-04-19 22:41:04 +0700

    lab10

--- lab10/lab10.c
+++ lab10/lab10.c
@@ -7,33 +7,35 @@
 #define ERROR_FORK -1
 #define ERROR_WAIT -1
 #define CHILD_PROCESS 0
+#define MIN_NUM_OF_ARGS 2
+#define STATUS_INIT 0
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
+    if (argc < MIN_NUM_OF_ARGS) {
         printf("Usage: %s program_name [program_arg0 ...]\n", argv[0]);
-        return 0;
+        return EXIT_SUCCESS;
     }
 
     pid_t fork_check = fork();
     if (fork_check == ERROR_FORK) {
         perror("Error while creating process");
-        return 0;
+        return EXIT_FAILURE;
     }
 
     if (fork_check == CHILD_PROCESS) {
         execvp(argv[1], &argv[1]);
         perror("execvp error");
-        return 0;
+        return EXIT_FAILURE;
     }
 
-    int status = 0;
+    int status = STATUS_INIT;
     pid_t wait_check = wait(&status);
     if (wait_check == ERROR_WAIT) {
         perror("wait error");
-        return 0;
+        return EXIT_FAILURE;
     }
 
     int exit_status = WEXITSTATUS(status);
     printf("Child process exit status: %d\n", exit_status);
-    return 0;
+    return EXIT_SUCCESS;
 }
