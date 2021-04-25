61cfbe82108429f00e7cdc5f838b06e05ec0e71b
VitalTerrorX <vitalterrorx@gmail.com>
2021-04-25 13:49:07 +0700

    lab19

--- lab19/lab19.c
+++ lab19/lab19.c
@@ -1,5 +1,3 @@
-#define _FILE_OFFSET_BITS 64
-
 #include <stdio.h>
 #include <sys/types.h>
 #include <limits.h>
@@ -7,99 +5,125 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <fnmatch.h>
+#include <unistd.h>
 
-#define SIZE (NAME_MAX + 1)
+#define ERROR_OPEN_DIR NULL
+#define ERROR_CLOSE_DIR -1
+#define ERROR_FIND_MATCHES -1
+#define ERROR_GET_PATTERN -1
+#define ERROR_READ -1
+#define ERROR_WRITE -1
+
+#define SUCCESS_CLOSE_DIR 0
+#define SUCCESS_GET_PATTERN 0
+
+#define PATTERN_SIZE NAME_MAX
 #define NO_ERROR 0
+#define FNM_MATCH 0
+#define NOT_ALL_ENTRIES_CHECKED 1
 
 extern int errno;
 
-int main(int argc, char **argv) {
-    char pattern[SIZE];
-    struct dirent *dp;
-    DIR *dirp;
-    int i = 0, length, count = 0, j, patternLength;
-    int flag = 0;
+int find_matching_entries(DIR *dirp, char *pattern) {
+    int matches_count = 0;
+    while (NOT_ALL_ENTRIES_CHECKED) {
+        struct dirent *directory_entry = readdir(dirp);
+        if (directory_entry == NULL) {
+            if (errno != NO_ERROR) {
+                perror("Can't read directory");
+                return ERROR_FIND_MATCHES;
+            }
+            break;
+        }
 
-    dirp = opendir(".");
-    if (dirp == NULL) {
-         perror("Can't open directory");
-         exit(0);
+        int fnmatch_check = fnmatch(pattern, directory_entry->d_name, FNM_PATHNAME);
+        switch(fnmatch_check) {
+            case FNM_MATCH:
+                printf("%s\n", directory_entry->d_name);
+                matches_count++;
+                break;
+            case FNM_NOMATCH:
+                break;
+            default:
+                fprintf(stderr, "Error occurred in fnmatch\n");
+                return ERROR_FIND_MATCHES;
+        }
+    }
+    return matches_count;
+}
+
+ssize_t get_pattern(char *pattern, size_t size) {
+    ssize_t write_check = write(STDOUT_FILENO, "Enter pattern: ", 15);
+    if (write_check == ERROR_WRITE) {
+        perror("Error while writing message for user");
+        return ERROR_GET_PATTERN;
     }
 
-    printf("Enter pattern: ");
-    fgets(pattern, SIZE, stdin);
-    patternLength = strlen(pattern) - 1;
-    pattern[patternLength] = 0;
+    ssize_t bytes_read = read(STDIN_FILENO, pattern, size);
+    if (bytes_read == ERROR_READ) {
+        perror("Error while reading from stdin");
+        return ERROR_GET_PATTERN;
+    }
+    if (pattern[bytes_read - 1] == '\n') {
+        pattern[bytes_read - 1] = '\0';
+        bytes_read--;
+    }
 
-    for (i = 0; i < patternLength; i++) {
+    for (int i = 0; i < bytes_read; i++) {
         if (pattern[i] == '/') {
-            printf("Symbol '/' is not supported pattern\n");
-            exit(0);
+            printf("Symbol \'/\' is not supported in the pattern\n");
+            return ERROR_GET_PATTERN;
         }
     }
 
-    do {
-	dp = readdir(dirp);
-	if (dp == NULL) {
-		if (errno != NO_ERROR) {
-			perror("Can't read directory");
-		}
-		break;
-	}
+    return bytes_read;
+}
 
-        length = strlen(dp->d_name);
-        j = 0;
-        flag = 0;
+DIR *open_directory(const char *dirname) {
+    DIR *dirp = opendir(".");
+    if (dirp == NULL) {
+        perror("Can't open directory");
+        return ERROR_OPEN_DIR;
+    }
+    return dirp;
+}
 
-        for (i = 0; (length > i) && (patternLength > j); i++) {
-            if (('?' != pattern[j]) && ('*' != pattern[j])) {
-                if (pattern[j] != dp->d_name[i]) break;
-                j++;
-            } else if ('?' == pattern[j]) {
-                j++;
-            } else {
-                while (patternLength > j) {
-                    j++;
-                    if ('*' != pattern[j]) break;
-                }
-                if (patternLength == j) {
-                    flag = 1;
-                    break;
-                }
-                if (pattern[j] == '?') {
-                    j++;
-                    continue;
-                }
-                while (length > i) {
-                    if (pattern[j] == dp->d_name[i]) {
-                        break;
-                    }
-                    i++;
-                }
-                j++;
-            }
-        }
+int close_directory(DIR *dirp) {
+    int close_check = closedir(dirp);
+    if (close_check == ERROR_CLOSE_DIR) {
+        perror("Can't close directory");
+        return ERROR_CLOSE_DIR;
+    }
+    return SUCCESS_CLOSE_DIR;
+}
 
-        if (length == i) {
-            while (patternLength > j) {
-                if ('*' != pattern[j])
-                    break;
-                j++;
-            }
-            if (patternLength == j) flag = 1;
-        }
+int main(int argc, char **argv) {
+    DIR *dirp = open_directory(".");
+    if (dirp == ERROR_OPEN_DIR) {
+        return EXIT_FAILURE;
+    }
 
-        if ((1 == flag) || (i == length && j == patternLength)) {
-            printf("%s\n", dp->d_name);
-            count++;
-        }
+    char pattern[PATTERN_SIZE + 1];
+    ssize_t pattern_length = get_pattern(pattern, PATTERN_SIZE);
+    if (pattern_length == ERROR_GET_PATTERN) {
+        return EXIT_FAILURE;
+    }
+    pattern[pattern_length] = '\0';
 
-    } while (1);
-
-    if (count == 0) {
+    int matched_entries_count = find_matching_entries(dirp, pattern);
+    if (matched_entries_count == ERROR_FIND_MATCHES) {
+        close_directory(dirp);
+        return EXIT_FAILURE;
+    }
+    if (matched_entries_count == 0) {
         printf("Pattern was = %s\n", pattern);
     }
-    closedir(dirp);
-    return 0;
+
+    int close_check = close_directory(dirp);
+    if (close_check == ERROR_CLOSE_DIR) {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
 
