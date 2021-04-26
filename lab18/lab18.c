d6ddd0874f50f3ec4b3761afb7cc3a008a4b5364
VitalTerrorX <vitalterrorx@gmail.com>
2021-04-26 16:42:38 +0700

    lab18

--- lab18/lab18.c
+++ lab18/lab18.c
@@ -1,80 +1,120 @@
 #include <stdio.h>
+#include <sys/types.h>
 #include <stdlib.h>
-#include <dirent.h>
-#include <pwd.h>
-#include <grp.h>
 #include <sys/stat.h>
+#include <grp.h>
+#include <pwd.h>
 #include <time.h>
+#include <string.h>
 #include <unistd.h>
 #include <libgen.h>
 
-#define STAT_VALID 0
+#define ERROR_PRINT_FILE_INFO -1
+#define ERROR_STAT -1
 
-char *default_args[] = { ".", NULL };
+#define SUCCESS_PRINT_FILE_INFO 0
 
-void print_stats(char *name, struct stat *stat) {
-	char mode[11] = { 0 };
-	
-	if (S_ISREG(stat->st_mode)) {
-		mode[0] = '-';
-	}
-	else if (S_ISDIR(stat->st_mode)) {
-		mode[0] = 'd';
-	}
-	else {
-		mode[0] = '?';
-	}
+#define NUMBER_OF_FILE_PERMISSIONS 9
+#define NUMBER_OF_PERMISSIONS 3
 
-    	// user
-    	mode[1] = (stat->st_mode & S_IRUSR) ? 'r' : '-';
-    	mode[2] = (stat->st_mode & S_IWUSR) ? 'w' : '-';
-    	mode[3] = (stat->st_mode & S_IXUSR) ? 'x' : '-';
+#define TRUE 1
+#define FALSE 0
 
-    	// group
-    	mode[4] = (stat->st_mode & S_IRGRP) ? 'r' : '-';
-    	mode[5] = (stat->st_mode & S_IWGRP) ? 'w' : '-';
-    	mode[6] = (stat->st_mode & S_IXGRP) ? 'x' : '-';
+#define NO_INFO "?"
+#define CURRENT_DIRECTORY "."
 
-    	// others
-    	mode[7] = (stat->st_mode & S_IROTH) ? 'r' : '-';
-    	mode[8] = (stat->st_mode & S_IWOTH) ? 'w' : '-';
-    	mode[9] = (stat->st_mode & S_IXOTH) ? 'x' : '-';
+int file_has_permission(struct stat *statbuf, mode_t permission) {
+    if ((statbuf->st_mode & permission) != FALSE) {
+        return TRUE;
+    }
+    return FALSE;
+}
 
-    	mode[10] = 0;
+void get_file_permissions(struct stat *statbuf, char *permissions_string) {
+    char permission_symbols[NUMBER_OF_PERMISSIONS] = {'r', 'w', 'x'};
+    int file_permissions[NUMBER_OF_FILE_PERMISSIONS] = {S_IRUSR, S_IWUSR, S_IXUSR,
+                                                        S_IRGRP, S_IWGRP, S_IXGRP,
+                                                        S_IROTH, S_IWOTH, S_IXOTH};
 
-	printf("%s", mode);
-	printf("\t%lu", stat->st_nlink);
+    for (int perm_idx = 0; perm_idx < NUMBER_OF_FILE_PERMISSIONS; perm_idx++) {
+        if (file_has_permission(statbuf, file_permissions[perm_idx]) == TRUE) {
+            permissions_string[perm_idx] = permission_symbols[perm_idx % NUMBER_OF_PERMISSIONS];
+        } else {
+            permissions_string[perm_idx] = '-';
+        }
+    }
+}
 
-	struct passwd* uent = getpwuid(stat->st_uid);
-	struct group* gent = getgrgid(stat->st_gid);
+char *get_file_owner(struct stat *statbuf) {
+    struct passwd *owner_info = getpwuid(statbuf->st_uid);
+    if (owner_info == NULL) {
+        perror("Can't get file owner info");
+        return NO_INFO;
+    }
+    return owner_info->pw_name;
+}
 
-    	if (uent == NULL)
-		printf("\t%d", stat->st_uid);
-    	else
-		printf("\t%s", uent->pw_name);
+char *get_file_group(struct stat *statbuf) {
+    struct group *group_info = getgrgid(statbuf->st_gid);
+    if (group_info == NULL) {
+        perror("Can't get file group info");
+        return NO_INFO;
+    }
+    return group_info->gr_name;
+}
 
-    	if (gent == NULL)
-		printf("\t%d", stat->st_gid);
-    	else
-		printf("\t%s", gent->gr_name);
-	
-	char lbuf[FILENAME_MAX] = { 0 };
-	char fname[FILENAME_MAX];
-    	strftime(lbuf, FILENAME_MAX, "%H:%M %e %b %Y", localtime(&(stat->st_ctime)));
-    	printf("\t%lu\t%s\t%s\n", stat->st_size, lbuf, basename(name));
+char *get_file_modification_time(struct stat *statbuf) {
+    char *time = ctime(&statbuf->st_mtime);
+    if (time == NULL) {
+        perror("Can't get file timestamp");
+        exit(EXIT_FAILURE);
+    }
+    return time;
+}
+
+char get_file_type(struct stat *statbuf) {
+    switch (statbuf->st_mode & S_IFMT) {
+        case S_IFREG:
+            return '-';
+        case S_IFDIR:
+            return 'd';
+        default:
+            return '?';
+    }
+}
+
+void print_file_info(char *filename) {
+    struct stat statbuf;
+    int stat_check = stat(filename, &statbuf);
+    if (stat_check == ERROR_STAT) {
+        perror(filename);
+        return;
+    }
+
+    char permissions_string[NUMBER_OF_FILE_PERMISSIONS + 1];
+    get_file_permissions(&statbuf, permissions_string);
+    permissions_string[NUMBER_OF_FILE_PERMISSIONS] = '\0';
+
+    printf("%c%s   %lu %s %s    %ld %.19s %s\n",
+           get_file_type(&statbuf),
+           permissions_string,
+           statbuf.st_nlink,            //number of links
+           get_file_owner(&statbuf),
+           get_file_group(&statbuf),
+           statbuf.st_size,             //file size
+           get_file_modification_time(&statbuf),
+           basename(filename));         //get last component of path
 }
 
 int main(int argc, char **argv) {
-	char **args = (argc == 1) ? default_args : (argv + 1);
-	for (int i = 0; args[i] != NULL; i++) {
-		struct stat stat;
-		int get_stat_res = lstat(args[i], &stat);
-		if (get_stat_res == STAT_VALID) {
-			print_stats(args[i], &stat);
-		}
-		else {
-			perror(args[i]);
-		}
-	}
-	return 0;
+    if (argc < 2) {
+        print_file_info(CURRENT_DIRECTORY);
+        return EXIT_SUCCESS;
+    }
+
+    for (int file_idx = 1; file_idx < argc; file_idx++) {
+        print_file_info(argv[file_idx]);
+    }
+
+    return EXIT_SUCCESS;
 }
