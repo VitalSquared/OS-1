22ff2650fe430564d5872f8bfc06153a34bb100f
VitalTerrorX <vitalterrorx@gmail.com>
2021-05-01 15:33:23 +0700

    lab19

--- lab19/lab19.c
+++ lab19/lab19.c
@@ -24,6 +24,7 @@
 #define FNM_MATCH 0
 #define NOT_ALL_ENTRIES_CHECKED 1
 #define NO_ENTRY NULL
+#define NO_FLAGS 0
 
 #define CURRENT_DIRECTORY "."
 
@@ -32,6 +33,7 @@ extern int errno;
 int find_matching_entries(DIR *dirp, char *pattern) {
     int matches_count = 0;
     while (NOT_ALL_ENTRIES_CHECKED) {
+        errno = NO_ERROR;
         struct dirent *directory_entry = readdir(dirp);
         if (directory_entry == NO_ENTRY) {
             if (errno != NO_ERROR) {
@@ -41,7 +43,7 @@ int find_matching_entries(DIR *dirp, char *pattern) {
             break;
         }
 
-        int fnmatch_check = fnmatch(pattern, directory_entry->d_name, FNM_PATHNAME);
+        int fnmatch_check = fnmatch(pattern, directory_entry->d_name, NO_FLAGS);
         switch(fnmatch_check) {
             case FNM_MATCH:
                 printf("%s\n", directory_entry->d_name);
@@ -85,6 +87,19 @@ ssize_t get_pattern(char *pattern, size_t size) {
     return bytes_read;
 }
 
+ssize_t modify_pattern(char *modified_pattern, char *entered_pattern, ssize_t entered_pattern_length) {
+    ssize_t current_modified_pattern_position = 0;
+    for (ssize_t i = 0; i < entered_pattern_length; i++) {
+        if (entered_pattern[i] == '[' || entered_pattern[i] == ']' || entered_pattern[i] == '\\') {
+            modified_pattern[current_modified_pattern_position] = '\\';
+            current_modified_pattern_position++;
+        }
+        modified_pattern[current_modified_pattern_position] = entered_pattern[i];
+        current_modified_pattern_position++;
+    }
+    return current_modified_pattern_position;
+}
+
 DIR *open_directory(const char *dirname) {
     DIR *dirp = opendir(dirname);
     if (dirp == NULL) {
@@ -109,21 +124,25 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    char pattern[PATTERN_SIZE + 1];
-    ssize_t pattern_length = get_pattern(pattern, PATTERN_SIZE);
-    if (pattern_length == ERROR_GET_PATTERN) {
+    char entered_pattern[PATTERN_SIZE + 1];
+    ssize_t entered_pattern_length = get_pattern(entered_pattern, PATTERN_SIZE);
+    if (entered_pattern_length == ERROR_GET_PATTERN) {
         close_directory(dirp);
         return EXIT_FAILURE;
     }
-    pattern[pattern_length] = '\0';
+    entered_pattern[entered_pattern_length] = '\0';
 
-    int matched_entries_count = find_matching_entries(dirp, pattern);
+    char modified_pattern[2 * PATTERN_SIZE + 1];
+    ssize_t modified_pattern_length = modify_pattern(modified_pattern, entered_pattern, entered_pattern_length);
+    modified_pattern[modified_pattern_length] = '\0';
+
+    int matched_entries_count = find_matching_entries(dirp, modified_pattern);
     if (matched_entries_count == ERROR_FIND_MATCHES) {
         close_directory(dirp);
         return EXIT_FAILURE;
     }
     if (matched_entries_count == 0) {
-        printf("Pattern was = %s\n", pattern);
+        printf("Pattern was = %s\n", entered_pattern);
     }
 
     int close_check = close_directory(dirp);
@@ -132,4 +151,3 @@ int main(int argc, char **argv) {
     }
     return EXIT_SUCCESS;
 }
-
